bf65b3381df896da4e8a0c67262dbd4043febce7
kjtkanaly <kjtkanaly@gmail.com>
2024-09-29 22:44:28 -0400

    Added an item text box and updated the text box script

--- Scripts/UI/TextBox.cs
+++ Scripts/UI/TextBox.cs
@@ -7,7 +7,7 @@ public partial class TextBox : CanvasLayer
     // Game Componenets
     // Public
     [Export] public float charactersPerSecond = 0.05f;
-    [Export] public string testText;
+    public string testText = "The quick brown fox jumps over the lazy dog";
     [Export] public Texture2D testImage;
 
     // Protected
@@ -46,14 +46,14 @@ public partial class TextBox : CanvasLayer
     {
         Visible = false;
         label.Text = "";
-        textRect.Texture = null;
+        SetIcon(null);
     }
 
     private void ShowTextBox(string text, Texture2D icon)
     {
         Visible = true;
         label.Text = text;
-        textRect.Texture = icon;
+        SetIcon(icon);
 
         // Animate the Text
         label.VisibleRatio = 0;
@@ -63,6 +63,14 @@ public partial class TextBox : CanvasLayer
             "visible_ratio", 
             1, 
             label.Text.Length * charactersPerSecond);
+    }
+
+    private void SetIcon(Texture2D icon) {
+        if (textRect == null) {
+            return;
+        }
+
+        textRect.Texture = icon;
     }
 
     //-------------------------------------------------------------------------
