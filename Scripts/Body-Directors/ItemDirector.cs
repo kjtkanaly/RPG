7a5ea0775db5cf828328ce54a9afb77867c9da50
kjtkanaly <kjtkanaly@gmail.com>
2024-10-02 23:08:33 -0400

    Changed the item body director to be static body 2d instead of area 2d

--- Scripts/Body-Directors/ItemDirector.cs
+++ Scripts/Body-Directors/ItemDirector.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class ItemDirector : Area2D
+public partial class ItemDirector : StaticBody2D
 {
     //-------------------------------------------------------------------------
     // Game Componenets
