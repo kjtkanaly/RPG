624a87140299ca3147ccba7068ffc94f044eb052
kjtkanaly <kjtkanaly@gmail.com>
2024-11-10 18:55:30 -0500

    Updated the Update Instances python and the red guy data

--- Scripts/Resources/Character-Data/UpdateInstances.py
+++ Scripts/Resources/Character-Data/UpdateInstances.py
@@ -1,21 +1,28 @@
 import json
+import argparse
 
 def main():
-    templatePath = "Resources/Character-Data/template.json"
-    filePath = "Resources/Character-Data/Allies/dummy.json"
+    # Get the Input Arguements
+    args = GetInputArgs()
 
+    # Get the Template and instance file path's from the inputs args
+    templatePath = args.template
+    filePath = args.inputFile
+
+    # Read in the JSON files as string
     rawTemplateData = GetFileAsString(templatePath)
     rawFileData = GetFileAsString(filePath)
 
+    # Convert the JSON string to a JSON object
     templateData = json.loads(rawTemplateData)
     instanceData = json.loads(rawFileData)
 
+    # Update the instance object to the template
     UpdateInstanceToTemplate(templateData, instanceData)
 
     # Save the updated instance
     with open(filePath, "w") as outfile: 
         json.dump(instanceData, outfile, indent=4)
-
 
 def UpdateInstanceToTemplate(template, instance):
     
@@ -34,6 +41,12 @@ def GetFileAsString(filePath):
     with open(filePath, 'r') as file:
         fileContent = file.read()
     return fileContent
+
+def GetInputArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-inputFile', help='The character file that you want to update')
+    parser.add_argument('-template', help='The reference template')
+    return parser.parse_args()
 
 if __name__ == "__main__":
     main()
